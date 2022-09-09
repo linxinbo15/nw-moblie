c23538ae6051c91a547085a6a28e560f82ac65bf
newwave <33818889+linxinbo15@users.noreply.github.com>
2022-09-09 22:53:25 +0800

    Update demo.ts

--- src/api/demo.ts
+++ src/api/demo.ts
@@ -3,7 +3,7 @@ import { UrlEnum } from '@/utils/httpEnum';
 // 获取秘钥 organiz/encryption/key
 export function encryptionKey() {
   return request({
-    url: UrlEnum.API + 'organiz/encryption/key',
+    url: UrlEnum.API + '',
     method: 'post',
   });
 }
